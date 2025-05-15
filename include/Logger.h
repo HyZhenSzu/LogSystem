ebf66dfd7cd0714a70f5e8bfca59c0cdc8ece7df
Hyz <13592815435@163.com>
2025-05-15 13:42:39 +0800

    add 5 macros in Logger

--- include/Logger.h
+++ include/Logger.h
@@ -6,6 +6,21 @@
 namespace Hyzhen{
 namespace Utility{
 
+    #define debug(format, ...) \
+        Logger::getInstance()->log(Logger::LogLevel::DEBUG, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
+    #define info(format, ...) \
+        Logger::getInstance()->log(Logger::LogLevel::INFO, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
+    #define warning(format, ...) \
+        Logger::getInstance()->log(Logger::LogLevel::WARNING, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
+    #define error(format, ...) \
+        Logger::getInstance()->log(Logger::LogLevel::ERROR, __FILE__, __LINE__, format, ##__VA_ARGS__);
+        
+    #define fatal(format, ...) \
+        Logger::getInstance()->log(Logger::LogLevel::FATAL, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
     // In order to make sure that there is only one log system running to avoid conflicts, 
     // the "Logger" class needs to be in the form of a singleton.
     class Logger
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,15 @@ using namespace Hyzhen::Utility;
 int main()
 {
     Logger::getInstance()->open("./test.log");
-    Logger::getInstance()->log(Logger::LogLevel::DEBUG, __FILE__, __LINE__, "Hello World!");
-    Logger::getInstance()->log(Logger::LogLevel::DEBUG, __FILE__, __LINE__, "My name is Hyzhen!");
+    // Logger::getInstance()->log(Logger::LogLevel::DEBUG, __FILE__, __LINE__, "Hello World!");
+    // Logger::getInstance()->log(Logger::LogLevel::DEBUG, __FILE__, __LINE__, "My name is Hyzhen!");
+
+    debug("Hello World! This is a debug message.");
+    info("My name is Hyzhen. This is a info.");
+    warning("I always see warnings when coding.");
+    error("I hate errors, but we can't avoid them.");
+    fatal("fatal errors, I don't want to see them.");
+
     Logger::getInstance()->close();
     return 0;
 }
